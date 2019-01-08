03ed9bdad680513a2190ec577fcbf6d7cbbc60b0
Eduardo Yubero Lavandeira <eduyuberol@gmail.com>
2019-01-08 18:49:40 +0100

    init project

---
+++ app/src/main/java/com/master/eyubero/pfg/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.master.eyubero.pfg
+
+
+/**
+ * Created by Edu Yube ┌(▀Ĺ̯ ▀-͠ )┐
+ * on 08/01/2019(ノಠ益ಠ)ノ
+ */
+import android.content.Intent
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    private var msGoToMain = 2500
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            goToMain()
+            finish()
+        }, msGoToMain.toLong())
+    }
+
+    private fun goToMain() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
