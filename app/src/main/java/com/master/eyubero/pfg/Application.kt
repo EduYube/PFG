76abe75ce81c505f8ba2f66babd58c8b59727a26
Eduardo Yubero Lavandeira <eduyuberol@gmail.com>
2019-02-03 12:27:33 +0100

    ranking model added

--- app/src/main/java/com/master/eyubero/pfg/Application.kt
+++ app/src/main/java/com/master/eyubero/pfg/Application.kt
@@ -3,22 +3,31 @@ package com.master.eyubero.pfg
 import android.app.Application
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.master.eyubero.pfg.model.RankingModel
 import com.master.eyubero.pfg.model.SportModel
 import com.master.eyubero.pfg.model.TeamModel
 import com.master.eyubero.pfg.model.UserModel
+import java.lang.Math.random
+import java.util.*
+import java.util.EnumSet.range
+import kotlin.math.nextUp
 
 
 /**
  * Created by Edu Yube ┌(▀Ĺ̯ ▀-͠ )┐
  * on 01/02/2019(ノಠ益ಠ)ノ
  */
-class Application: Application() {
+class Application : Application() {
 
     lateinit var mSportRef: DatabaseReference
     lateinit var database: FirebaseDatabase
     val sports = ArrayList<String>()
 
+    val teamList = ArrayList<TeamModel>()
+    val rankingList = ArrayList<RankingModel>()
+
     override fun onCreate() {
+
         super.onCreate()
         sports.add(0, "baloncesto")
         sports.add(1, "balonmano")
@@ -32,15 +41,17 @@ class Application: Application() {
 
 
     private fun initDataBases() {
+
         database = FirebaseDatabase.getInstance()
         initSportsDB()
         initUserDB()
     }
 
     private fun initUserDB() {
+
         val mUserRef = database.getReference("users")
 
-        val admin = UserModel(0,"Yubero",true)
+        val admin = UserModel(0, "Yubero", true)
         mUserRef.child(admin.user).setValue(admin)
     }
 
@@ -49,18 +60,27 @@ class Application: Application() {
         mSportRef = database.getReference("sports")
         for (sportId in 0 until sports.size) {
 
-            val sport = SportModel(sportId, sports[sportId], initTeamsDB())
+            val sport = SportModel(sportId, sports[sportId], initTeamsDB(), initRankingDB())
             mSportRef.child("$sportId-${sport.name}").setValue(sport)
         }
     }
 
     private fun initTeamsDB(): ArrayList<TeamModel> {
-        val teamList = ArrayList<TeamModel>()
+
         for (teamId in 1..5) {
 
             val team = TeamModel(teamId, "team $teamId")
             teamList.add(team)
         }
         return teamList
+    }
+
+    private fun initRankingDB(): ArrayList<RankingModel> {
+
+        for (team in 0 until teamList.size) {
+            val ranking = RankingModel(team+1,teamList[team],Random().nextInt(21-0))
+            rankingList.add(ranking)
+        }
+        return rankingList
     }
 }
--- app/src/main/java/com/master/eyubero/pfg/model/RankingModel.kt
+++ app/src/main/java/com/master/eyubero/pfg/model/RankingModel.kt
@@ -4,4 +4,4 @@ package com.master.eyubero.pfg.model
  * Created by Edu Yube ┌(▀Ĺ̯ ▀-͠ )┐
  * on 01/02/2019(ノಠ益ಠ)ノ
  */
-class RankingModel(val teams: TeamModel)
+class RankingModel(val position: Int, val team: TeamModel, val points: Int)
