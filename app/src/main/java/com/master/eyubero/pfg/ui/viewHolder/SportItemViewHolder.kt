807914b270ecdf09ad9ddc7feb55b971478d3935
Eduardo Yubero Lavandeira <eduyuberol@gmail.com>
2019-02-05 02:22:27 +0100

    added backgound to items

--- app/src/main/java/com/master/eyubero/pfg/ui/viewHolder/SportItemViewHolder.kt
+++ app/src/main/java/com/master/eyubero/pfg/ui/viewHolder/SportItemViewHolder.kt
@@ -15,19 +15,34 @@ import com.master.eyubero.pfg.model.SportModel
 
 class SportItemViewHolder(val mBinding: LayoutItemBinding) : RecyclerView.ViewHolder(mBinding.root) {
 
-    fun bind(sport: SportModel, listener: onSportItemClickListener){
+    fun bind(sport: SportModel, listener: onSportItemClickListener) {
 
         itemView.setOnClickListener {
             listener.onSportItemClick(sport)
         }
 
-        mBinding.tvSportName.text = sport.name
-        when(sport.id){
-            0 -> mBinding.ivSportIcon.setImageResource(R.drawable.ic_basketball)
-            1 -> mBinding.ivSportIcon.setImageResource(R.drawable.ic_handball)
-            2 -> mBinding.ivSportIcon.setImageResource(R.drawable.ic_football)
-            3 -> mBinding.ivSportIcon.setImageResource(R.drawable.ic_rugby)
-            4 -> mBinding.ivSportIcon.setImageResource(R.drawable.ic_volleyball)
+        mBinding.tvSportName.text = sport.name!!.capitalize()
+        when (sport.id) {
+            0 -> {
+                mBinding.ivSportIcon.setImageResource(R.drawable.ic_basketball)
+                mBinding.clItem.background = mBinding.root.context.resources.getDrawable(R.drawable.bg_basket)
+            }
+            1 -> {
+                mBinding.ivSportIcon.setImageResource(R.drawable.ic_handball)
+                mBinding.clItem.background = mBinding.root.context.resources.getDrawable(R.drawable.bg_handball)
+            }
+            2 -> {
+                mBinding.ivSportIcon.setImageResource(R.drawable.ic_football)
+                mBinding.clItem.background = mBinding.root.context.resources.getDrawable(R.drawable.bg_football)
+            }
+            3 -> {
+                mBinding.ivSportIcon.setImageResource(R.drawable.ic_rugby)
+                mBinding.clItem.background = mBinding.root.context.resources.getDrawable(R.drawable.bg_rugby)
+            }
+            4 -> {
+                mBinding.ivSportIcon.setImageResource(R.drawable.ic_volleyball)
+                mBinding.clItem.background = mBinding.root.context.resources.getDrawable(R.drawable.bg_voley)
+            }
         }
 
     }
