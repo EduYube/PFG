9e6986049facb4d91f444e45be9fe93b08db3b6d
Eduardo Yubero Lavandeira <eduyuberol@gmail.com>
2019-02-05 18:38:29 +0100

    init rankingBinding

--- app/src/main/java/com/master/eyubero/pfg/ui/fragment/SportRankingFragment.kt
+++ app/src/main/java/com/master/eyubero/pfg/ui/fragment/SportRankingFragment.kt
@@ -1,5 +1,8 @@
 package com.master.eyubero.pfg.ui.fragment
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -7,23 +10,32 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.master.eyubero.pfg.R
+import com.master.eyubero.pfg.databinding.FragmentSportRankingBinding
 import com.master.eyubero.pfg.model.RankingModel
+import com.master.eyubero.pfg.ui.viewModel.ResultsViewModel
 
 
 class SportRankingFragment : Fragment() {
     private var rankingList: List<RankingModel>? = null
+    private lateinit var mViewModel: ResultsViewModel
+    private lateinit var mBinding: FragmentSportRankingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            rankingList = it.get("ranking") as List<RankingModel>
-        }
+
+
+        mViewModel = ViewModelProviders.of(this).get(ResultsViewModel::class.java)
+        mViewModel.getData().observe(this, Observer {
+            for (i in 0 until it!!.size){
+                rankingList = it[i].ranking
+            }
+        })
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-
-        return inflater.inflate(R.layout.fragment_sport_ranking, container, false)
+        mBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_sport_ranking,container,false)
+        return mBinding.root
     }
 
     companion object {
